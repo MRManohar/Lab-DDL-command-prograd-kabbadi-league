966caa2cf2cb3bff4f168d9604627bd0dcc13056
MRManohar <lmmrider99@gmail.com>
2020-07-29 14:06:42 +0530

    FSJ0420A109

--- ddl-pkl.sql
+++ ddl-pkl.sql
@@ -1,28 +1,31 @@
 -- PROGRESSION - 1
 
-
 -- 1. **Create table city**
 create table city(
-ID int primary key,
+ID int,
 name varchar(50)
 );
 -- 2. **Create table referee**
 create table referee as(select * from city);
 
 -- 3. **Create table innings**
-create table referee as(select * from city);
-
--- 4. **Create table extra_type**
 create table innings(
-id int primary key,
+id int,
 innings_number int
 );
+
+-- 4. **Create table extra_type**
+create table extra_type(
+id int,
+name varchar(50)
+);
+
 -- 5. **Create table skill**
-create table skills as(select * from city);
+create table skill as(select * from city);
 
 -- 6. **Create table team**
 create table team(
-id int primary key,
+id int,
 name varchar(50),
 coach varchar(50),
 home_city int,
@@ -31,7 +34,7 @@ captain int
 
 -- 7. **Create table player**
 create table player(
-id int primary key,
+id int,
 name varchar(50),
 country varchar(50),
 skill_Id int,
@@ -39,17 +42,17 @@ team_Id int
 );
 -- 8. **Create table venue**
 create table venue(
-id int primary key,
+id int,
 stadium_name varchar(50),
 city_id int
 );
 -- 9. **Create table event**
 create table event(
-id int primary key,
-innings_Id int,
+id int,
+innings_id int,
 event_no int,
-raider_Id int,
-raid_Points int,
+raider_id int,
+raid_points int,
 defending_points int,
 clock_in_seconds int,
 team_one_score int,
@@ -59,7 +62,7 @@ team_two_score int
 
 -- 10. **Create table extra_event**
 create table extra_event(
-id int primary key,
+id int,
 event_id int,
 extra_type_id int,
 points int,
@@ -67,7 +70,7 @@ scoring_teaming_id int
 );
 -- 11. **Create table outcome**
 create table outcome(
-id int primary key,
+id int,
 status varchar(100),
 winner_team_id int,
 score int,
@@ -75,7 +78,7 @@ player_of_match int
 );
 -- 12. **Create table game**
 create table game(
-id int primary key,
+id int,
 game_date DATE,
 team_id_1 int,
 team_id_2 int,
